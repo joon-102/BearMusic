2856995e531428a9d2cbb908c9d32c8626416b3a
joon-102 <joon_1@outlook.kr>
2024-11-13 22:10:52 +0900

    ➕ 백그라운드 이미지에 트랙 정보를 추가

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import { imageService  } from './services/imageService'
+import { imageService } from './services/imageService'
+import sharp from 'sharp';
 
-(async()=>{
+(async () => {
     const dd = new imageService()
 
-    const color = (await dd.extractPastelColors("https://image.bugsm.co.kr/album/images/original/9968/996863.jpg?version=undefined"))
+    const color = (await dd.extractPastelColors("https://image.bugsm.co.kr/album/images/original/40833/4083370.jpg?version=undefined"))
+    if (color == null) return
+
+
+    const Background = await dd.createBackgroundImage(2560, 1440, color.startColor, color.endColor);
+    const ddd = await dd.createAlbumImage('https://image.bugsm.co.kr/album/images/original/40833/4083370.jpg?version=undefined', Background);
+
+    const ddd123123: any = dd.addTrackInfo(2560, 1440, ddd, '친구로 지내다 보면', 'BIG Naughty', '친구로 지내다 보면')
+
+
+    sharp(await ddd123123)
+        .toFile('dd.png')
+
+
 
 })();
--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,42 +1,147 @@
 import Vibrant from 'node-vibrant';
+import sharp from 'sharp';
+import axios from 'axios';
 
 export class imageService {
 
     private adjustToPastel(rgb: number[]): number[] {
         const [r, g, b] = rgb;
-        return [Math.min(255, r + (255 - r) * 0.4), Math.min(255, g + (255 - g) * 0.4), Math.min(255, b + (255 - b) * 0.4)]
+
+        return [
+            Math.min(280, r + (280 - r) * 0.5),
+            Math.min(280, g + (280 - g) * 0.5),
+            Math.min(280, b + (280 - b) * 0.5)
+        ];
     }
 
-    private contrast(rgb: number[], color: number[]) {
-        const lum1 = 0.2126 * Math.pow(rgb[0] / 255, 2.2) + 0.7152 * Math.pow(rgb[1] / 255, 2.2) + 0.0722 * Math.pow(rgb[2] / 255, 2.2);
-        const lum2 = 0.2126 * Math.pow(color[0] / 255, 2.2) + 0.7152 * Math.pow(color[1] / 255, 2.2) + 0.0722 * Math.pow(color[2] / 255, 2.2);
-        return (lum1 + 0.05) / (lum2 + 0.05);
+    private contrast(rgb: number[], color: number[]): number {
+        return (0.2126 * Math.pow(rgb[0] / 255, 2.2) + 0.7152 * Math.pow(rgb[1] / 255, 2.2) + 0.0722 * Math.pow(rgb[2] / 255, 2.2) + 0.05) / (0.2126 * Math.pow(color[0] / 255, 2.2) + 0.7152 * Math.pow(color[1] / 255, 2.2) + 0.0722 * Math.pow(color[2] / 255, 2.2) + 0.05);
     }
 
-    private toRgbString = (color: [number, number, number]): string => {
-        return `rgb(${Math.round(color[0])}, ${Math.round(color[1])}, ${Math.round(color[2])})`;
-    };
+    private isStrongColor(rgb: number[]): boolean {
+        const [r, g, b] = rgb;
+        return (0.2126 * r + 0.7152 * g + 0.0722 * b) > 200;
+    }
 
-    async extractPastelColors(imgSrc: string): Promise<any> {
+    async extractPastelColors(imgSrc: string): Promise<{ startColor: number[]; endColor: number[] } | null> {
+
         const palette: any = await Vibrant.from(imgSrc).getPalette();
 
         if (palette && palette.Vibrant && palette.Muted) {
-            let startColor = palette.LightVibrant ? palette.LightVibrant.getRgb() : palette.Vibrant.getRgb();
-            let endColor = palette.DarkVibrant ? palette.DarkVibrant.getRgb() : palette.Muted.getRgb();
+            let startColor = palette.LightVibrant
+                ? palette.LightVibrant.getRgb()
+                : palette.Vibrant.getRgb();
+            let endColor = palette.DarkVibrant
+                ? palette.DarkVibrant.getRgb()
+                : palette.Muted.getRgb();
+
+            if (this.isStrongColor(startColor)) {
+                startColor = palette.Muted ? palette.Muted.getRgb() : palette.LightVibrant.getRgb();
+            }
+
+            if (this.isStrongColor(endColor)) {
+                endColor = palette.Muted ? palette.Muted.getRgb() : palette.LightVibrant.getRgb();
+            }
 
             startColor = this.adjustToPastel(startColor);
             endColor = this.adjustToPastel(endColor);
 
-            const white = [255, 255, 255];
-
-            const contrast1 = this.contrast(startColor, white);
-            const contrast2 = this.contrast(endColor, white);
+            const contrast1 = this.contrast(startColor, [255, 255, 255]);
+            const contrast2 = this.contrast(endColor, [255, 255, 255]);
 
             return {
-                startColor: (contrast1 > contrast2 ? startColor : endColor),
-                endColor: (contrast1 > contrast2 ? endColor : startColor)
+                startColor: contrast1 > contrast2 ? startColor : endColor,
+                endColor: contrast1 > contrast2 ? endColor : startColor
             };
         }
         return null;
     }
+
+
+    async createBackgroundImage(width: number, height: number, startColor: number[], endColor: number[]): Promise<Buffer> {
+
+        const grayStartX: number = 180.5;
+        const grayEndY: number = height - Math.tan((40 * Math.PI) / 180) * (width - grayStartX);
+
+        return await sharp({ create: { width: width, height: height, channels: 3, background: { r: startColor[0], g: startColor[1], b: startColor[2] } } })
+            .composite([{
+                input: Buffer.from(
+                    `
+                    <svg width="${width}" height="${height}">
+                        <polygon points="${grayStartX},${height} ${width},${grayEndY} ${width},${height}" fill="rgb(${endColor[0]},${endColor[1]},${endColor[2]})" />
+                    </svg>
+                `
+                ),
+                blend: 'over',
+            }])
+             .toFormat('png')
+            .toBuffer();
+    }
+
+    async createAlbumImage(imgSrc: string, background: Buffer): Promise<Buffer> {
+
+        const response = await axios.get(imgSrc, { responseType: 'arraybuffer' });
+        const album: Buffer = Buffer.from(response.data, 'binary');
+
+        return await sharp(background)
+            .composite([{
+                input: await sharp(album).resize(1125, 1125, { fit: 'cover' }).toBuffer(),
+                top: 157,
+                left: 155,
+                blend: 'over',
+            }])
+            .toFormat('png')
+            .toBuffer();
+    }
+
+    async addTrackInfo(width: number, height: number, AlbumImage: Buffer, title: string, artist: string, album: string) {
+
+        const rectX = 1270;
+        const rectY = 275;
+        const rectWidth = 1280;
+        const rectHeight = 200;
+
+        const titleSize = 62;
+        const artistSize = 50;
+        const albumSize = 42;
+
+        const artistHeight = 80 + 5;
+        const albumHeight = 60;
+
+        return sharp(AlbumImage)
+            .composite([
+                {
+                    input: Buffer.from(`
+                        <svg width="${width}" height="${height}">
+                            <rect x="${rectX + rectWidth / 2}" y="${rectY + rectHeight / 2 + titleSize / 2}" width="${rectWidth}" height="${rectHeight}" fill="none" stroke="none"/>
+                            <text x="${rectX + rectWidth / 2}" y="${rectY + rectHeight / 2 + titleSize / 2}" font-family="GmarketSansMedium" font-size="${titleSize}" fill="White" text-anchor="middle" dominant-baseline="middle">${title}</text>
+                        </svg>
+                        `),
+                    gravity: 'northwest'
+                },
+                {
+                    input: Buffer.from(`
+                        <svg width="${width}" height="${height}">
+                            <rect x="${rectX + rectWidth / 2}" y="${rectY + rectHeight / 2 + artistSize / 2 + artistHeight}" width="${rectWidth}" height="${rectHeight}" fill="none" stroke="none"/>
+                            <text x="${rectX + rectWidth / 2}" y="${rectY + rectHeight / 2 + artistSize / 2 + artistHeight}" font-family="GmarketSansMedium" font-size="${artistSize}" fill="White" text-anchor="middle" dominant-baseline="middle">${artist}</text>
+                        </svg>
+                        `),
+                    gravity: 'northwest'
+                },
+                {
+                    input: Buffer.from(`
+                        <svg width="${width}" height="${height}">
+                            <rect x="${rectX + rectWidth / 2}" y="${rectY + rectHeight / 2 + albumSize / 2 + (albumHeight + artistHeight)}" width="${rectWidth}" height="${rectHeight}" fill="none" stroke="none"/>
+                            <text x="${rectX + rectWidth / 2}" y="${rectY + rectHeight / 2 + albumSize / 2 + (albumHeight + artistHeight)}" font-family="GmarketSansMedium" font-size="${albumSize}" fill="White" text-anchor="middle" dominant-baseline="middle">[ ${album} ]</text>
+                        </svg>
+                        `),
+                    gravity: 'northwest'
+                }
+            ])
+            .toFormat('png')
+            .toBuffer();
+
+    }
+
+
 };
