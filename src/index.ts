6c8d8df3c90dfb894ca757e3b76ebf8904c01cc9
joon-102 <joon_1@outlook.kr>
2024-11-17 03:23:45 +0900

    ♻️ 비디오 생성 로직을 BearMusic 클래스에 통합

--- src/index.ts
+++ src/index.ts
@@ -4,38 +4,56 @@ import { audioService } from './services/audioService';
 import { videoService } from './services/videoService';
 import * as dotenv from 'dotenv';
 
+class BearMusic {
+    image: imageService;
+    bugs: bugsService;
+    audio: audioService;
+    video: videoService;
+    trackInfo?: { track: string; artist: string; trackId: number; albumId: number; album: string; release: string; imgSrc: string };
+
+    constructor() {
+        this.image = new imageService();
+        this.bugs = new bugsService();
+        this.audio = new audioService();
+        this.video = new videoService();
+    }
+
+    async createVideoTrack(ALBUM_ID : number) : Promise<{ code : string , path : string }> {
+        const trackInfo = await this.bugs.getTrackInfo(ALBUM_ID);
+        if (trackInfo == null) return { code : 'ERR_TRACK_NOT_FOUND' , path : '' }
+
+        const Background: Buffer = await this.image.createBackgroundImage(trackInfo.imgSrc);
+        const trackImg: Buffer = await this.image.addTrackInfo(await this.image.createAlbumImage(trackInfo.imgSrc, Background), trackInfo.track, trackInfo.artist, trackInfo.album)
+
+        const Videoyt = await this.audio.getVideoyt(trackInfo.trackId);
+        if (Videoyt == null) return { code : 'ERR_VIDEO_NOT_FOUND' , path : '' }
+
+        await this.audio.downloadYouTubeAsMP3(Videoyt, `${trackInfo.track} - ${trackInfo.artist}`);
+
+        const sinklyrics = await this.bugs.getSinklyrics(trackInfo.trackId);
+        if (sinklyrics == null) return { code : 'ERR_LYRICS_NOT_FOUND' , path : '' }
+
+        sinklyrics.unshift({ time: 0, lyrics: '● ● ●' });
+
+        const lyricsImage = await this.image.tempLyricsToImage(trackImg, sinklyrics);
+
+        const VideoWithoutAudio = await this.video.generateVideoWithoutAudio(lyricsImage.lyricsList)
+
+        await this.video.addAudioToVideo(VideoWithoutAudio, `${trackInfo.track} - ${trackInfo.artist}`)
+
+        VideoWithoutAudio.tempFile.removeCallback();
+        lyricsImage.tempFile.forEach((element) => {
+            element.removeCallback();
+        });
+
+        return { code : 'SUCCESS_VIDEO_CREATED' , path : `out/${trackInfo.track} - ${trackInfo.artist}.mp4` }
+    }
+}
+
 (async () => {
     dotenv.config();
+    const app = new BearMusic()
 
-    const image = new imageService();
-    const bugs = new bugsService();
-    const audio = new audioService();
-    const video = new videoService();
-
-    const trackInfo = await bugs.getTrackInfo(32660867);
-
-    const Background: Buffer = await image.createBackgroundImage(trackInfo.imgSrc);
-    const trackImg: Buffer = await image.addTrackInfo(await image.createAlbumImage(trackInfo.imgSrc, Background), trackInfo.track, trackInfo.artist, trackInfo.album)
-
-    const Videoyt = await audio.getVideoyt(trackInfo.trackId);
-    if (Videoyt == null) return
-
-    await audio.downloadYouTubeAsMP3(Videoyt, `${trackInfo.track} - ${trackInfo.artist}`);
-
-    const sinklyrics = await bugs.getSinklyrics(trackInfo.trackId);
-    if (sinklyrics == null) return;
-
-    sinklyrics.unshift({ time: 0, lyrics: '● ● ●' });
-
-    const lyricsImage = await image.tempLyricsToImage(trackImg, sinklyrics);
-
-    const VideoWithoutAudio = await video.generateVideoWithoutAudio(lyricsImage.lyricsList)
-
-    await video.addAudioToVideo(VideoWithoutAudio, `${trackInfo.track} - ${trackInfo.artist}`)
-
-    VideoWithoutAudio.tempFile.removeCallback();
-    lyricsImage.tempFile.forEach((element) => {
-        element.removeCallback();
-    });
+    await app.createVideoTrack(32660867)
 
 })();
