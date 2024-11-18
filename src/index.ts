686d047a8d26e27a87a4930956ec27dd823ac8dc
joon-102 <joon_1@outlook.kr>
2024-11-19 01:17:01 +0900

    ➕ 비디오 업로드

--- src/index.ts
+++ src/index.ts
@@ -2,68 +2,160 @@ import { bugsService } from './services/bugsService';
 import { imageService } from './services/imageService';
 import { audioService } from './services/audioService';
 import { videoService } from './services/videoService';
+
+import Add from './models/Add'
+import Trash from './models/Trash'
+
+import timers from 'node:timers/promises'
+import * as YouTubeUploader from 'youtube-videos-uploader';
 import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 class BearMusic {
     image: imageService;
     bugs: bugsService;
     audio: audioService;
     video: videoService;
-    trackInfo?: { track: string; artist: string; trackId: number; albumId: number; album: string; release: string; imgSrc: string };
+    trackInfo?: { rawTrack: string; rawArtist: string; track: string; artist: string; trackId: number; albumId: number; album: string; release: string; imgSrc: string; };
 
     constructor() {
         this.image = new imageService();
-        this.bugs = new bugsService();
+        this.bugs = new bugsService(this);
         this.audio = new audioService();
         this.video = new videoService();
     }
 
+    async debug(message: string) {
+        console.debug("\x1b[32m%s\x1b[0m", `[ DEBUG ] ${message}`);
+    }
+
+    async system(message: string) {
+        console.debug("\x1b[34m%s\x1b[0m", `[ SYSTEM ] ${message}`);
+    }    
+
+    async uploadToYouTube(videoPath: string): Promise<{ code: string, path: string, error?: string }> {
+        try {
+            let isSuccess: boolean = false;
+            const video = [{
+                path: videoPath,
+                title: `${this.trackInfo?.rawTrack} - ${this.trackInfo?.rawArtist} | [가사/lyrics] `,
+                description: `🎶 본 영상은 가사 자막 영상입니다, 수익 창출은 되지 않습니다.\n\n🎧 Title : ${this.trackInfo?.rawTrack}\n🎤 Artist : ${this.trackInfo?.rawArtist}\n💿 Album : ${this.trackInfo?.album}\n📅 Release : ${this.trackInfo?.release.replace(/^(\d{4})(\d{2})(\d{2})$/, '$1년$2월$3일')}\n\n✨ Made by BearMusic`,
+                language: 'korean',
+                onSuccess: () => { isSuccess = true },
+                skipProcessingWait: true,
+                publishType: "PUBLIC",
+                isNotForKid: true,
+                uploadAsDraft: false,
+            }];
+
+            const upload = await (YouTubeUploader as any).upload({ email: process.env.GOODLE_ACCOUNT_EMAIL, pass: process.env.GOODLE_ACCOUNT_PASS }, video, { headless: false })
+            if (isSuccess) {
+                return { code: 'SUCCESS_VIDEO_UPLOAD', path: upload[0] }
+            } else {
+                return { code: 'ERR_VIDEO_UPLOAD', path: '', }
+            }
+        } catch (error) {
+            return { code: 'ERR_VIDEO_UPLOAD', path: '', error: String(error) }
+        }
+    }
+
     async createVideoTrack(ALBUM_ID: number): Promise<{ code: string, path: string }> {
-        console.debug("Launching BearMusic...")
-        const trackInfo = await this.bugs.getTrackInfo(ALBUM_ID);
-        if (trackInfo == null) return { code: 'ERR_TRACK_NOT_FOUND', path: '' }
+        this.debug("Launching BearMusic...")
 
-        const Background: Buffer = await this.image.createBackgroundImage(trackInfo.imgSrc);
-        console.debug(`  >> create background image`)
-        const trackImg: Buffer = await this.image.addTrackInfo(await this.image.createAlbumImage(trackInfo.imgSrc, Background), trackInfo.track, trackInfo.artist, trackInfo.album)
-        console.debug(`  >> create track image`)
+        try {
+            const trackInfo = await this.bugs.getTrackInfo(ALBUM_ID);
+            if (trackInfo == null) return { code: 'ERR_TRACK_NOT_FOUND', path: '' }
 
-        const Videoyt = await this.audio.getVideoyt(trackInfo.trackId);
-        if (Videoyt == null) return { code: 'ERR_VIDEO_NOT_FOUND', path: '' }
-        console.debug(`  >> https://www.youtube.com/watch?v=${Videoyt} - Find Track YouTube sound source`)
+            this.trackInfo = trackInfo;
 
-        await this.audio.downloadYouTubeAsMP3(Videoyt, `${trackInfo.track} - ${trackInfo.artist}`);
-        console.debug(`  >> https://www.youtube.com/watch?v=${Videoyt} - Download YouTube sound source`)
+            const Background: Buffer = await this.image.createBackgroundImage(trackInfo.imgSrc);
+            this.debug(`  >> create background image`)
+            const trackImg: Buffer = await this.image.addTrackInfo(await this.image.createAlbumImage(trackInfo.imgSrc, Background), trackInfo.track, trackInfo.artist, trackInfo.album)
+            this.debug(`  >> create track image`)
 
-        const sinklyrics = await this.bugs.getSinklyrics(trackInfo.trackId);
-        if (sinklyrics == null) return { code: 'ERR_LYRICS_NOT_FOUND', path: '' }
-        console.debug(`  >> Get Sink lyrics`)
+            const Videoyt = await this.audio.getVideoyt(trackInfo.trackId);
+            if (Videoyt == null) return { code: 'ERR_VIDEO_NOT_FOUND', path: '' }
+            this.debug(`  >> https://www.youtube.com/watch?v=${Videoyt} - Find Track YouTube sound source`)
 
-        sinklyrics.unshift({ time: 0, lyrics: '● ● ●' });
+            await this.audio.downloadYouTubeAsMP3(Videoyt, `${trackInfo.track} - ${trackInfo.artist}`);
+            this.debug(`  >> https://www.youtube.com/watch?v=${Videoyt} - Download YouTube sound source`)
 
-        console.debug("Start creating a video...")
-        const lyricsImage = await this.image.tempLyricsToImage(trackImg, sinklyrics);
-        console.debug(" >> Successful creation of lyric image")
-        const VideoWithoutAudio = await this.video.generateVideoWithoutAudio(lyricsImage.lyricsList)
-        console.debug(" >> Successfully created a video without audio")
+            const sinklyrics = await this.bugs.getSinklyrics(trackInfo.trackId);
+            if (sinklyrics == null) return { code: 'ERR_LYRICS_NOT_FOUND', path: '' }
+            this.debug(`  >> Get Sink lyrics`)
 
-        console.debug(" >> Start audio synthesis...")
-        await this.video.addAudioToVideo(VideoWithoutAudio, `${trackInfo.track} - ${trackInfo.artist}`)
-        console.log("Successful audio synthesis")
+            sinklyrics.unshift({ time: 0, lyrics: '● ● ●' });
 
-        VideoWithoutAudio.tempFile.removeCallback();
-        lyricsImage.tempFile.forEach((element) => {
-            element.removeCallback();
-        });
+            this.debug("Start creating a video...")
+            const lyricsImage = await this.image.tempLyricsToImage(trackImg, sinklyrics);
+            this.debug("  >> Successful creation of lyric image")
+            const VideoWithoutAudio = await this.video.generateVideoWithoutAudio(lyricsImage.lyricsList)
+            this.debug("  >> Successfully created a video without audio")
 
-        console.debug("BearMusic successfully launched")
-        return { code: 'SUCCESS_VIDEO_CREATED', path: `out/${trackInfo.track} - ${trackInfo.artist}.mp4` }
+            this.debug("  >> Start audio synthesis...")
+            await this.video.addAudioToVideo(VideoWithoutAudio, `${trackInfo.track} - ${trackInfo.artist}`)
+            this.debug("Successful audio synthesis")
+
+            VideoWithoutAudio.tempFile.removeCallback();
+            lyricsImage.tempFile.forEach((element) => {
+                element.removeCallback();
+            });
+
+            this.debug("BearMusic successfully launched")
+            return { code: 'SUCCESS_VIDEO_CREATED', path: `out/${trackInfo.track} - ${trackInfo.artist}.mp4` }
+        } catch (error) {
+            return { code: 'ERR_VIDEO_NOT_GENERATION', path: `` }
+        }
     }
 }
 
 (async () => {
     dotenv.config();
-    const app = new BearMusic()
+    mongoose.connect(String(process.env.MONGO_URI));
 
-    await app.createVideoTrack(31548135)
+    while (true) {
+        const app = new BearMusic()
+
+        let Timeout = 1000 * 60 * 30;
+        const playlist = await Add.find().exec();
+
+        if (playlist.length <= 0) return Timeout = 1000 * 60;
+        const track = Number(playlist[0].trackId);
+
+        const video = await app.createVideoTrack(track);
+
+        if (video.code == "ERR_VIDEO_NOT_GENERATION") {
+            app.system(`ERR 알 수 없는 이유로 비디오를 생성하지 못했습니다. 30분 뒤 다시 시도합니다..\n`)
+            return Timeout = 1000 * 60 * 30;
+        }
+
+        if (video.code == "ERR_VIDEO_NOT_FOUND") {
+            app.system(`ERR YT 비디오를 찾지 못했습니다.\n`)
+            return Timeout = 1000 * 60;
+        }
+
+        if (video.code == "ERR_LYRICS_NOT_FOUND") {
+            app.system(`ERR 싱크 가사가 등록되어 있지 않습니다.\n`)
+            return Timeout = 1000 * 60;
+        }
+
+        if (video.code == "SUCCESS_VIDEO_CREATED") {
+            app.system(`비디오가 성공적으로 생성되었습니다.\n`)
+
+            const upload = await app.uploadToYouTube(video.path);
+
+            if (upload.code == 'ERR_VIDEO_UPLOAD') {
+                app.system(`\n비디오를 업로드하지 못했습니다. 3시간 뒤 다시 시도합니다..`)
+                return Timeout = 1000 * 60 * 60 * 3;
+            }
+
+            await new Trash({ title: app.trackInfo?.track, artist: app.trackInfo?.rawArtist, album: app.trackInfo?.album, trackId: app.trackInfo?.trackId }).save();
+
+            app.system(`비디오를 성공적으로 업로드 하였습니다. 1시간 뒤 프로세스를 반복합니다.`);
+            app.system(`업로드 정보\n🔗 Youtube : ${upload.path}\n🎧 Title : ${app.trackInfo?.rawTrack}\n🎤 Artist : ${app.trackInfo?.rawArtist}\n💿 Album : ${app.trackInfo?.album}\n📅 Release : ${app.trackInfo?.release.replace(/^(\d{4})(\d{2})(\d{2})$/, '$1년$2월$3일')}`)
+            return Timeout = 1000 * 60 * 60;
+        }
+
+        await timers.setTimeout(Timeout)
+    }
 })();
