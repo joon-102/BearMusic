80cadc31e77bd41e1faed7a8422766b87fdc6f7b
joon-102 <joon_1@outlook.kr>
2025-08-30 19:45:27 +0900

    🎨 대시보드 라우터 제거 및 관련 코드 정리

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
-import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
-import dashboardRouter from './panel/panel';
 import { startWorker } from './services/Worker';
 
 dotenv.config();
-
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use('/', dashboardRouter);
 
 (async () => {
   await mongoose.connect(String(process.env.MONGO_URI));
   await startWorker();
 })();
-
-app.listen(process.env.PORT, () => {
-  console.log(`[Panel] 패널 실행 중: http://localhost:${process.env.PORT}`);
-});
